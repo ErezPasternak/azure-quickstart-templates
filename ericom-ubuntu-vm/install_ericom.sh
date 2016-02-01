b541f81dae1eaae71f8647b271efb9e957e4cab9
ErezPasternak <erez.pasternak@gmail.com>
2016-02-01 13:18:01 +0200

    added time before each command

--- ericom-ubuntu-vm/install_ericom.sh
+++ ericom-ubuntu-vm/install_ericom.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # copy this to the ubunto machine to install necessary pre-requisites
-# do sudo bash ConnectPrerequisites.sh
 
 # domain to join
 DOMAIN=%1
@@ -20,23 +19,25 @@ echo "RemoteAgentAddress: $RemoteAgentAddress"
 echo "TenantInfo: $TenantInfo"
 
 # define variable of applicaiton to launch in the desktop.  can use xfce4-session or firefox for example
-sudo apt-get -y update
+time sudo apt-get -y update
 XRDP_APP=xfce4-session
 
-sudo apt-get -y install openssh-server
+time sudo apt-get -y install openssh-server
 
 # install firefix, 
 sudo apt-get -y install firefox
 
-# install and editor, for example emacs
-sudo apt-get -y install emacs
+# install chrome,
+time wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+time sudo dpkg -i google-chrome-stable_current_amd64.deb
+time sudo apt-get -y --force-yes install -f
 
 # install xfce window manager
-sudo apt-get -y install xfce4 xfce4-goodies
+time sudo apt-get -y install xfce4 xfce4-goodies
 
 # get xrdp and set the launch variable in startwm.sh
-sudo apt-get -y install xrdp
-sudo perl -pi.bak -E"s/^.*Xsession$/$XRDP_APP/"   /etc/xrdp/startwm.sh 
+time sudo apt-get -y install xrdp
+time sudo perl -pi.bak -E"s/^.*Xsession$/$XRDP_APP/"   /etc/xrdp/startwm.sh 
 
 # install likewise for AD support
 if [ ! -f likewise-open_6.1.0.406-0ubuntu5.1_amd64.deb ]
@@ -55,15 +56,15 @@ then
   wget http://de.archive.ubuntu.com/ubuntu/pool/universe/l/likewise-open/likewise-open-gui_6.1.0.406-0ubuntu5.1_amd64.deb 
 fi
 
-sudo dpkg -i likewise-open_6.1.0.406-0ubuntu5.1_amd64.deb
-sudo dpkg -i libglade2-0_2.6.4-2_amd64.deb
-sudo dpkg -i likewise-open-gui_6.1.0.406-0ubuntu5.1_amd64.deb
+time sudo dpkg -i likewise-open_6.1.0.406-0ubuntu5.1_amd64.deb
+time sudo dpkg -i libglade2-0_2.6.4-2_amd64.deb
+time sudo dpkg -i likewise-open-gui_6.1.0.406-0ubuntu5.1_amd64.deb
 
 # install QT
-sudo apt-get install qt5-default
+time sudo apt-get install qt5-default
 
 #install unzip 
-apt-get install unzip
+time apt-get install unzip
 
 perl -pi.bak -E's/^hosts:.*files mdns4_minimal .NOTFOUND=return. dns$/hosts: files dns [NOTFOUND=return]/'   /etc/nsswitch.conf
 
@@ -71,8 +72,8 @@ perl -pi.bak -E's/^hosts:.*files mdns4_minimal .NOTFOUND=return. dns$/hosts: fil
 
 # append w/o using redirection
 
-sudo sed -i '$ a\allow-guest=false' /usr/share/lightdm/lightdm.conf.d/50-ubuntu.conf
-sudo sed -i '$ a\greeter-show-manual-login=true' /usr/share/lightdm/lightdm.conf.d/50-ubuntu.conf
+time sudo sed -i '$ a\allow-guest=false' /usr/share/lightdm/lightdm.conf.d/50-ubuntu.conf
+time sudo sed -i '$ a\greeter-show-manual-login=true' /usr/share/lightdm/lightdm.conf.d/50-ubuntu.conf
 
 domainjoin-cli join $DOMAIN $DOMAIN_ADMIN $DOMAIN_PWD
 
@@ -85,11 +86,11 @@ then
     
 fi
 
-unzip ericom-connect-remote-host_x64.deb.zip
-sudo dpkg –i ericom-connect-remote-host_x64.deb
+time unzip ericom-connect-remote-host_x64.deb.zip
+time sudo dpkg –i ericom-connect-remote-host_x64.deb
 
 #configure the remote agent 
-sudo /opt/ericom/ericom-connect-remote-agent/ericom-connect-remote-agent connect -server-url https://<$RAWSaddress>:8044 [-host-name $RemoteAgentAddress] [-tenant-info $TenantInfo]
+time sudo /opt/ericom/ericom-connect-remote-agent/ericom-connect-remote-agent connect -server-url https://<$RAWSaddress>:8044 [-host-name $RemoteAgentAddress] [-tenant-info $TenantInfo]
 
 
 echo REBOOT computer now with 'sudo reboot'
