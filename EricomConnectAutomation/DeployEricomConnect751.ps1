00e7b3d8ef86ae75fbafadbddf5431d59a625ba6
ErezPasternak <erez.pasternak@gmail.com>
2016-05-10 17:16:09 +0300

    Configure IE without security

--- EricomConnectAutomation/DeployEricomConnect751.ps1
+++ EricomConnectAutomation/DeployEricomConnect751.ps1
@@ -178,7 +178,16 @@ function Config-CreateGrid($config = $Settings)
 	Write-Output "Ericom Connect Grid configuration has been ended."
     
 }
-
+# remove the secrty settings of IE 
+function Disable-IEESC
+{
+	$AdminKey = “HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}”
+	$UserKey = “HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}”
+	Set-ItemProperty -Path $AdminKey -Name “IsInstalled” -Value 0
+	Set-ItemProperty -Path $UserKey -Name “IsInstalled” -Value 0
+	Stop-Process -Name Explorer
+	Write-Host “IE Enhanced Security Configuration (ESC) has been disabled.” -ForegroundColor Green
+}
 
 function Expand-ZIPFile($file, $destination)
 {
@@ -196,6 +205,7 @@ function ConfigureFirewall
 {
 	Import-Module NetSecurity
 	Set-NetFirewallProfile -Profile Domain -Enabled False
+	Disable-IEESC
 }
 #David - can we fix it for single machine install - just to add the Domain users to the local RemoteDesktopUsers ?
 
