232b3dfd86274a8a79063c6505dda951d339eaa3
ErezPasternak <erez.pasternak@gmail.com>
2016-05-22 16:45:53 +0300

    Moved less used variable to the do not touch part

--- EricomConnectAutomation/DeployEricomConnect751.ps1
+++ EricomConnectAutomation/DeployEricomConnect751.ps1
@@ -9,7 +9,6 @@ Import-Module BitsTransfer
 Write-Host "BitsTransfer Module is loaded"
 
 # Settings Section
-
 # Ericom Connect installer location   
 $InstallerName = "EricomConnectPOC.exe"
 $EC_download_url_or_unc = "https://www.ericom.com/demos/"+ $InstallerName 
@@ -23,27 +22,34 @@ $AdminPassword = "admin"
 # Ericom Connect Grid Setting
 $GridName = $env:computername + (Get-Random -Minimum 0 -Maximum 999)
 $HostOrIp = (Get-NetIPAddress -AddressFamily IPv4)[0].IPAddress # [System.Net.Dns]::GetHostByName((hostname)).HostName
-$SaUser = ""
-$SaPassword = ""
 $DatabaseServer = $env:computername+"\ERICOMCONNECTDB"
 $DatabaseName = $GridName
-$ConnectConfigurationToolPath = "\Ericom Software\Ericom Connect Configuration Tool\EricomConnectConfigurationTool.exe"
-$UseWinCredentials = "true"
 $LookUpHosts = $HostOrIp
 
 # E-mail Settings
 $To = "erez.pasternak@ericom.com"
+$externalFqdn = [System.Net.Dns]::GetHostByName((hostname)).HostName
+
+
+
+
+# Internal Code - DO NOT CHANGE  
+$global:adminApi = $null
+$global:adminSessionId = $null
+
+$ConnectConfigurationToolPath = "\Ericom Software\Ericom Connect Configuration Tool\EricomConnectConfigurationTool.exe"
+$UseWinCredentials = "true"
+$SaUser = ""
+$SaPassword = ""
+
+$emailTemplate = "WebServer\DaaS\emails\ready.html"
 $From = "daas@ericom.com"
 $SMTPServer = "ericom-com.mail.protection.outlook.com"
 $SMTPSUser = "daas@ericom.com"
 $SMTPassword = "1qaz@Wsx#"
 $SMTPPort = 25
-$externalFqdn = [System.Net.Dns]::GetHostByName((hostname)).HostName
 
-# Internal Code - DO NOT CHANGE  
-$global:adminApi = $null
-$global:adminSessionId = $null
-$emailTemplate = "WebServer\DaaS\emails\ready.html"
+
 function Start-EricomConnection
 {
 	$Assem = Import-EricomLib
@@ -1515,6 +1521,122 @@ function PostInstall
 }
 
 
+Function New-ConnectServer {
+
+$inputXml = @"
+<Window 
+        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
+        xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
+        xmlns:local="clr-namespace:NanoFinal"
+        Title="Ericom Connect Builder 0.9" Height="400" Width="525" ResizeMode="CanMinimize" WindowStartupLocation="CenterScreen" Cursor="Arrow" FontFamily="Tahoma">
+    <Grid Background="{DynamicResource {x:Static SystemColors.ActiveCaptionBrushKey}}" OpacityMask="{DynamicResource {x:Static SystemColors.GrayTextBrushKey}}">
+        <Label Name="IntroLabel" Content="Please fill in the required information. Then, press Deploy." HorizontalAlignment="Left" Margin="14,11,0,0" VerticalAlignment="Top" Width="349"/>
+        <TextBox Name="DomainName" HorizontalAlignment="Left" Height="23" Margin="129,41,0,0" TextWrapping="Wrap" Text="" VerticalAlignment="Top" Width="193"/>
+        <TextBox Name="AdminName" HorizontalAlignment="Left" Height="23" Margin="129,69,0,0" TextWrapping="Wrap" Text="" VerticalAlignment="Top" Width="193" RenderTransformOrigin="0.495,0.524"/>
+        <TextBox Name="AdminPassword" HorizontalAlignment="Left" Height="23" Margin="129,98,0,0" TextWrapping="Wrap" Text="" VerticalAlignment="Top" Width="193"/>
+        <TextBox Name="GridName" HorizontalAlignment="Left" Height="23" Margin="129,126,0,0" TextWrapping="Wrap" Text="" VerticalAlignment="Top" Width="193"/>
+        <TextBox Name="DatabaseServer" HorizontalAlignment="Left" Height="23" Margin="129,154,0,0" TextWrapping="Wrap" Text="" VerticalAlignment="Top" Width="193"/>
+        <TextBox Name="DatabaseName" HorizontalAlignment="Left" Margin="129,182,0,0" VerticalAlignment="Top" Width="193" Height="23"/>
+        <TextBox Name="EMail" HorizontalAlignment="Left" Margin="129,210,0,0" VerticalAlignment="Top" Width="193" Height="23"/>
+        <TextBox Name="DownloadPath" HorizontalAlignment="Left" Height="23" Margin="129,250,0,0" TextWrapping="Wrap" Text="" VerticalAlignment="Top" Width="333"/>
+        <TextBox Name="LocalPath" HorizontalAlignment="Left" Height="23" Margin="129,278,0,0" TextWrapping="Wrap" Text="" VerticalAlignment="Top" Width="333"/>
+        <TextBox Name="MSIName" HorizontalAlignment="Left" Height="23" Margin="129,306,0,0" TextWrapping="Wrap" Text="" VerticalAlignment="Top" Width="333"/>
+        <Label Name="DomainNameLabel" Content="Domain Name" HorizontalAlignment="Left" Margin="26,39,0,0" VerticalAlignment="Top"/>
+        <Label Name="NameLabel" Content="Admin Name" HorizontalAlignment="Left" Margin="26,68,0,0" VerticalAlignment="Top"/>
+        <Label Name="PasswordLabel" Content="Admin Password" HorizontalAlignment="Left" Margin="26,95,0,0" VerticalAlignment="Top"/>
+        <Label Name="GridLabel" Content="Grid Name" HorizontalAlignment="Left" Margin="26,124,0,0" VerticalAlignment="Top"/>
+        <Label Name="DBServerLabel" Content="Database Server" HorizontalAlignment="Left" Margin="26,153,0,0" VerticalAlignment="Top"/>
+        <Label Name="DNNameLabel" Content="Database Name" HorizontalAlignment="Left" Margin="26,181,0,0" VerticalAlignment="Top" Width="99"/>
+        <Label Name="MailLabel" Content="E-Mail" HorizontalAlignment="Left" Margin="26,210,0,0" VerticalAlignment="Top" Width="99"/>
+        <Label Name="DownloadPathLabel" Content="Download Path" HorizontalAlignment="Left" Margin="26,250,0,0" VerticalAlignment="Top"/>
+        <Label Name="LocalPathLabel" Content="Local Path" HorizontalAlignment="Left" Margin="26,279,0,0" VerticalAlignment="Top"/>
+        <Label Name="MSINameLabel" Content="Target Path" HorizontalAlignment="Left" Margin="26,306,0,0" VerticalAlignment="Top"/>
+        <Border BorderThickness="1" HorizontalAlignment="Left" Height="101" Margin="26,240,0,0" VerticalAlignment="Top" Width="447" Opacity="0.8">
+            <Border.BorderBrush>
+                <LinearGradientBrush EndPoint="0.5,1" StartPoint="0.5,0">
+                    <GradientStop Color="Black" Offset="0"/>
+                    <GradientStop Color="#FF3C618D" Offset="1"/>
+                </LinearGradientBrush>
+            </Border.BorderBrush>
+        </Border>
+        <CheckBox Name="checkBoxConfigureWindows" Content="Configure Windows" HorizontalAlignment="Left" Margin="381,55,0,0" VerticalAlignment="Top"/>
+        <CheckBox Name="checkBoxInstallEC" Content="InstallEC" HorizontalAlignment="Left" Margin="381,75,0,0" VerticalAlignment="Top"/>
+        <CheckBox Name="checkBoxCreateGrid" Content="CreateGrid" HorizontalAlignment="Left" Margin="381,95,0,0" VerticalAlignment="Top"/>
+        <CheckBox Name="checkBoxCreateUser" Content="Create Users" HorizontalAlignment="Left" Margin="381,115,0,0" VerticalAlignment="Top"/>
+        <CheckBox Name="checkBoxInstallApps" Content="Install Apps" HorizontalAlignment="Left" Margin="381,135,0,0" VerticalAlignment="Top"/>
+        <CheckBox Name="checkBoxPublishApps" Content="Publish Apps" HorizontalAlignment="Left" Margin="381,156,0,0" VerticalAlignment="Top"/>
+        <CheckBox Name="checkBoxSystem" Content="System Test" HorizontalAlignment="Left" Margin="381,176,0,0" VerticalAlignment="Top"/>
+        <Border BorderThickness="1" HorizontalAlignment="Left" Height="157" Margin="378,48,0,0" VerticalAlignment="Top" Width="130">
+            <Border.BorderBrush>
+                <LinearGradientBrush EndPoint="0.5,1" StartPoint="0.5,0">
+                    <GradientStop Color="Black" Offset="0"/>
+                    <GradientStop Color="#FF213A64" Offset="1"/>
+                </LinearGradientBrush>
+            </Border.BorderBrush>
+        </Border>
+        <Label Name="label" Content="Actions" HorizontalAlignment="Left" Margin="409,24,0,0" VerticalAlignment="Top" Width="63"/>
+        <Button Name="Deploy" Content="Deploy" HorizontalAlignment="Left" Margin="434,350,0,0" VerticalAlignment="Top" Width="75"/>
+    </Grid>
+</Window>
+"@
+
+    [void][System.Reflection.Assembly]::LoadWithPartialName('presentationframework')
+    [xml]$XAML = $inputXML
+    $reader=(New-Object System.Xml.XmlNodeReader $xaml) 
+    $Form=[Windows.Markup.XamlReader]::Load( $reader )
+    $xaml.SelectNodes("//*[@Name]") | %{Set-Variable -Name "WPF$($_.Name)" -Value $Form.FindName($_.Name)}
+
+    $WPFDomainName.text = $domainName
+    $WPFAdminName.text = $AdminUser
+    $WPFAdminPassword.text = $AdminPassword
+    $WPFGridName.text = $GridName
+    $WPFDatabaseServer.text = $DatabaseServer 
+    $WPFDatabaseName.text = $DatabaseName
+    $WPFDownloadPath.text = $EC_download_url_or_unc 
+    $WPFLocalPath.text    = $EC_local_path
+    $WPFMSIName.text      = $InstallerName
+    $WPFEMail.text        = $To
+    
+    #Button
+    $WPFDeploy.Add_Click({
+    
+    $global:domainName     = $WPFDomainName.text 
+    $global:AdminUser      = $WPFAdminName.text  
+    $global:AdminPassword  = $WPFAdminPassword.SecurePassword 
+    $global:GridName       = $WPFGridName.text  
+    $global:DatabaseServer = $WPFDatabaseServer.text  
+    $global:DatabaseName   = $WPFDatabaseName.text 
+    $global:EC_download_url_or_unc = $WPFDownloadPath.text  
+    $global:EC_local_path  = $WPFLocalPath.text    
+    $global:InstallerName  = $WPFMSIName.text       
+    $global:To             = $WPFEMail.text  
+    
+   
+    #Actions
+    $Actions = (((Get-Variable -Name *Checkbox*).Value -match "IsChecked:True")).Name
+
+    $ToInstall = Switch ($Actions){
+
+    "CHECKBOXSTORAGE" { "Microsoft-NanoServer-Storage-Package" }
+    "CHECKBOXCOMPUTE" { "Microsoft-NanoServer-Compute-Package"}
+    "CHECKBOXDEFENDER" { "Microsoft-NanoServer-Defender-Package"}
+    "CHECKBOXCLUSTERING" { "Microsoft-NanoServer-FailoverCluster-Package"}
+    "CHECKBOXCONTAINER" { "Microsoft-NanoServer-Containers-Package"}
+    "CHECKBOXDSC" { "Microsoft-NanoServer-DSC-Package"}
+    "CHECKBOXIIS" { "Microsoft-NanoServer-IIS-Package"}
+
+    }
+  
+
+    $form.Close()
+
+    })
+
+    $Form.ShowDialog() | Out-Null 
+
+}
 
 
 
@@ -1527,8 +1649,9 @@ function PostInstall
 
 
 # Main Code 
-
+#New-ConnectServer
 # Relaunch if we are not running as admin
+
 Invoke-RequireAdmin $script:MyInvocation
 
 # Prerequisite check 
