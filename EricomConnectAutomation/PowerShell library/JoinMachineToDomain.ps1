72adad918f5500c32ae1cbba984a5082e45858c5
ErezPasternak <erez.pasternak@gmail.com>
2016-05-26 14:55:48 +0300

    added useful script

---
+++ EricomConnectAutomation/PowerShell library/JoinMachineToDomain.ps1
@@ -0,0 +1,5 @@
+$domain = "myDomain"
+$password = "myPassword!" | ConvertTo-SecureString -asPlainText -Force
+$username = "$domain\myUserAccount" 
+$credential = New-Object System.Management.Automation.PSCredential($username,$password)
+Add-Computer -DomainName $domain -Credential $credential
